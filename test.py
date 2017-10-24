d2cf8b7f521098218cd679a61ceecb47309d4e37
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2017-10-24 20:24:15 +0200

    Added New Test

--- test.py
+++ test.py
@@ -6,8 +6,10 @@ import document
 def main():
     backup.perform_backup(os.getcwd() + '\\Tests\\test_backup', os.getcwd() + '\\Archive\\test_backup', None,
                           None, ['build'], True, None, False)
-    document.perform_document([os.getcwd() + '\\Tests\\test_document'], os.getcwd() + "\\Archive\\test_document", None,
+    document.perform_document([os.getcwd() + '\\Tests\\test_document_1'], os.getcwd() + "\\Archive\\test_document_1", None,
                               None, False, None, False, False)
+    document.perform_document([os.getcwd() + '\\Tests\\test_document_2'], os.getcwd() + "\\Archive\\test_document_2", None,
+                              None, True, None, False, True)
 
 if __name__ == '__main__':
     main()
